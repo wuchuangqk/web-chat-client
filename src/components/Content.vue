87765225671f5b019f2175cbe4e8a3f18d8c4f3e
吴闯 <wuchuang456@126.com>
2025-04-11 16:08:21 +0800

    调整“已复制”文本样式

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="flex mb-3" :style="{ 'justify-content': isSelf ? 'flex-end' : 'flex-start' }">
+    <!-- 对方头像 -->
     <Avatar v-if="!isSelf" :user="targetUser" class="mr-2" />
-    <div class="max-w-[80%] ">
+    <div>
+      <!-- 昵称 -->
       <p v-if="!isSelf" class=" text-xs text-[#999] mb-1">[{{ targetUser.type }}]{{ targetUser.name }}</p>
-      <div class="flex items-center" @click="copy">
-        <div v-if="isSelf && showCopy" class="text-[#999] mr-2 cursor-pointer text-xs">已复制</div>
+      <!-- 消息内容 -->
+      <div class="flex items-center relative" :class="[isSelf ? 'pl-11' : 'pr-11']" @click="copy">
         <div class="p-2 rounded bg-white break-all" :class="{ 'self': isSelf }" v-html="message.data"></div>
-        <div v-if="!isSelf && showCopy" class="text-[#999] ml-2 cursor-pointer text-xs">已复制</div>
+        <div v-if="showCopy" class="text-[#999] text-xs absolute" :class="[isSelf ? '-left-0' : '-right-0']">已复制</div>
       </div>
     </div>
+    <!-- 自己头像 -->
     <Avatar v-if="isSelf" :user="targetUser" class="ml-2" />
   </div>
 </template>
